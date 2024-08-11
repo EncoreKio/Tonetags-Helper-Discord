5ccaa45bd924ed5d922ff423c062c5bae18c6ea5
Kio <islemmaki11@gmail.com>
2024-08-11 04:11:34 +0200

    Meilleurs supports des tonetags disponibles

--- commands/help/help.js
+++ commands/help/help.js
@@ -1,64 +1,47 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder } = require('discord.js');
 const tonetags_json = require('../../tonetags.json');
+const tonetags_list = ['j', 's', 'srs', 'hj', 'p', 'r', 'l', 'ly', 't', 'nm', 'nc', 'neg', 'pc', 'pos', 'Ih', 'nbh', 'm', 'li', 'ij', 'rh', 'rt', 'genq', 'hyp', 'c', 'th', 'f', 'g']
+
+
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('help')
+        .setName('tonetags')
         .setDescription('Informations sur un tonetags !')
         .addStringOption(option => 
             option.setName('tonetags')
             .setDescription('Lequel ? ')
-            .setRequired(true)
-            .addChoices(
-                { "name": "/j", "value": "j" },
-                { "name": "/s", "value": "s" },
-                { "name": "/srs", "value": "srs" },
-                { "name": "/hj", "value": "hj" },
-                { "name": "/p", "value": "p" },
-                { "name": "/r", "value": "r" },
-                { "name": "/l", "value": "l" },
-                { "name": "/ly", "value": "ly" },
-                { "name": "/t", "value": "t" },
-                { "name": "/nm", "value": "nm" },
-                { "name": "/nc", "value": "nc" },
-                { "name": "/neg", "value": "neg" },
-                { "name": "/pc", "value": "pc" },
-                { "name": "/pos", "value": "pos" },
-                { "name": "/Ih", "value": "Ih" },
-                { "name": "/nbh", "value": "nbh" },
-                { "name": "/m", "value": "m" },
-                { "name": "/li", "value": "li" },
-                { "name": "/ij", "value": "ij" },
-                { "name": "/rh", "value": "rh" },
-                { "name": "/rt", "value": "rt" },
-                { "name": "/genq", "value": "genq" },
-                { "name": "/hyp", "value": "hyp" },
-                { "name": "/c", "value": "c" },
-                { "name": "/th", "value": "th" },
-            )
-        ),
+            .setRequired(true)),
+
     async execute(interaction) {
-        const tonetags = interaction.options.getString('tonetags');
+        const tonetags = interaction.options.getString('tonetags').toLowerCase();
         const embed = new EmbedBuilder()
             .setTitle(`__**🎲 ⦁  TonetagsHelp  /${tonetags}**__`)
             .setColor('#58d68d')
             .setFooter({ text: 'Informations sur les Tonetags | /credit ' });
 
-        const tonetags_choisi = tonetags_json[tonetags];
-
-        if (tonetags_choisi["example"]) {
-            embed.addFields(
-                { name: "Nom :", value: `${tonetags_choisi["nom"]}`, inline: false },
-                { name: "Description :", value: `${tonetags_choisi["description"]}`, inline: false },
-                { name: "Example", value: `${tonetags_choisi["example"]}`, inline: false }
-            );
+        if(tonetags_list.includes(tonetags)){
+            const tonetags_choisi = tonetags_json[tonetags];
+            if (tonetags_choisi["example"]) {
+                embed.addFields(
+                    { name: "Nom :", value: `${tonetags_choisi["nom"]}`, inline: false },
+                    { name: "Description :", value: `${tonetags_choisi["description"]}`, inline: false },
+                    { name: "Example", value: `${tonetags_choisi["example"]}`, inline: false }
+                );
+            } else {
+                embed.addFields(
+                    { name: "Nom :", value: `${tonetags_choisi["nom"]}`, inline: false },
+                    { name: "Description :", value: `${tonetags_choisi["description"]}`, inline: false }
+                );
+            }
         } else {
-            embed.addFields(
-                { name: "Nom :", value: `${tonetags_choisi["nom"]}`, inline: false },
-                { name: "Description :", value: `${tonetags_choisi["description"]}`, inline: false }
-            );
+            embed.setDescription(`${tonetags} n'est pas un tonetags supporté par le bot, vérifiez l'orthographe, si il est correct et n'est pas présent sur la liste suivante faites \`/tonetagssuggest\` pour proposer l'ajout d'un tonetags.\n\n Voici la liste des tonetags disponible : \`j, s, srs, hj, p, r, l, ly, t, nm, nc, neg, pc, pos, Ih, nbh, m, li, ij, rh, rt, genq, hyp, c, th, f, g\``);
         }
+
+       
+
+        
 
         await interaction.reply({ embeds: [embed] });
     },
